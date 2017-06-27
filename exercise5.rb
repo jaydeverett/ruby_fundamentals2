c9e1427b63d1b6a3c34783ba56832975a48be8da
Jay Deverett <jayjdeverett@gmail.com>
2017-06-27 14:04:53 -0400

    add exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,8 @@
+def greet_backwards(n)
+  n.reverse * 2
+end
+
+puts "Hello, #{greet_backwards("Bob")}! Welcome home."
+puts "Hello, #{greet_backwards("Shirly")}! Welcome home."
+puts "Hello, #{greet_backwards("Sue")}! Welcome home."
+puts "Hello, #{greet_backwards("Andy")}! Welcome home."
