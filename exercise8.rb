10c6aec95dd1ae28d6baa14049dd41d420a0bc87
Jay Deverett <jayjdeverett@gmail.com>
2017-06-28 19:36:07 -0400

    add exercise 8

---
+++ exercise8.rb
@@ -0,0 +1,12 @@
+expenses1 = [250, 7.95, 30.95, 16.50]
+
+expenses2 = [12, 123, 1234, 12345, 123456]
+
+def expense_sum(n)
+  sum = n.sum
+  return sum
+end
+
+puts expense_sum(expenses1)
+
+puts expense_sum(expenses2)
